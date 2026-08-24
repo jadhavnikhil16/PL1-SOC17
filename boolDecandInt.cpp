4d707eb40fb063897c8aafa0e92208705d63c080
Nikhil <nikhilvjadhav5566@gmail.com>
2026-08-24 12:58:55 +0530

    Add files via upload

---
+++ boolDecandInt.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;\
+
+int main()
+{
+bool isPassed;
+bool isActive;
+bool isValid;
+
+bool isAdult = true;
+bool hasLicense = false;
+
+bool defaultBool;
+
+cout<<"isAdult: "<<isAdult<<endl;
+cout<<"hasLicense: "<<hasLicense<<endl;
+cout<<"defaultBool:"<<defaultBool<<endl;
+
+return 0;
+}
+
